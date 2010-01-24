3bce5b8981428775dac7385167adf99bbf874022
Paul Irish <paul.irish@gmail.com>
2010-01-25 02:13:39 +0800

    formatting

--- pi.imgloaded.js
+++ pi.imgloaded.js
@@ -4,25 +4,23 @@
 
 $.fn.imagesLoaded = function() {
   var
-		args = arguments,
-		elems = this.filter('img'),
+    args = arguments,
+    elems = this.filter('img'),
     elemsLen = elems.length - 1,
-		fireOne = args.length === 2,
-		callback = args[1] || args[0];
+    fireOne = args.length === 2,
+    callback = args[1] || args[0];
 
   elems
-		.bind('load', function(e) {
-			if (fireOne) {
-				 !elemsLen-- && callback.call(elems, e);
-			}
-			else {
-				callback.call(this, e);
-			}
-		})
-		.each(function() {
-      // cached images don't fire load sometimes, so we reset src.
-      if (this.complete || this.complete === undefined){
-				this.src = this.src;
-			}
-  });
+    .bind('load', function(e) {
+        if (fireOne) {
+            !elemsLen-- && callback.call(elems, e);
+        } else {
+            callback.call(this, e);
+        }
+    }).each(function() {
+        // cached images don't fire load sometimes, so we reset src.
+        if (this.complete || this.complete === undefined){
+            this.src = this.src;
+        }
+    });
 }
