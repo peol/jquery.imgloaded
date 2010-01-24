5b405f72fdb1ab1be09ed964e8ff5c6e9b88fc8d
peol <peolanha@gmail.com>
2010-01-25 00:10:48 +0100

    Reverted some stuff, and made it a special 'imgload' event instead, all-images-loaded event not implemented through this approach.

--- index.php
+++ index.php
@@ -22,24 +22,36 @@
 <h1>Image load event</h1>
 <?php $random = rand(1, 2500) * rand(1, 2500); ?>
 
-<h2>Run callback when all images is loaded</h2>
-<ul id="all">
-	<li><p><span class="head delay">Delay</span><span class="head loaded">Loaded</span><span class="head img">Image</span></p></li>
-	<li><p><span class="delay">500ms</span><span class="loaded">false</span><span class="img"><img src="imgloader.php?delay=500&<?php print $random; ?>" alt="Image 500ms delay"/></span></p></li>
-	<li><p><span class="delay">1000ms</span><span class="loaded">false</span><span class="img"><img src="imgloader.php?delay=1000&<?php print $random; ?>" alt="Image 1000ms delay"/></span></p></li>
-	<li><p><span class="delay">1500ms</span><span class="loaded">false</span><span class="img"><img src="imgloader.php?delay=1500&<?php print $random; ?>" alt="Image 1500ms delay"/></span></p></li>
-	<li><p><span class="delay">2000ms</span><span class="loaded">false</span><span class="img"><img src="imgloader.php?delay=2000&<?php print $random; ?>" alt="Image 2000ms delay"/></span></p></li>
-	<li><p><span class="delay">2500ms</span><span class="loaded">false</span><span class="img"><img src="imgloader.php?delay=2500&<?php print $random; ?>" alt="Image 2500ms delay"/></span></p></li>
-</ul>
-
-<h2>Run callback when each image is loaded</h2>
+<h2>Run callback when each image is loaded (adds green background)</h2>
 <ul id="each">
-	<li><p><span class="head delay">Delay</span><span class="head loaded">Loaded</span><span class="head img">Image</span></p></li>
-	<li><p><span class="delay">500ms</span><span class="loaded">false</span><span class="img"><img src="imgloader.php?delay=500&<?php print $random; ?>" alt="Image 500ms delay"/></span></p></li>
-	<li><p><span class="delay">1000ms</span><span class="loaded">false</span><span class="img"><img src="imgloader.php?delay=1000&<?php print $random; ?>" alt="Image 1000ms delay"/></span></p></li>
-	<li><p><span class="delay">1500ms</span><span class="loaded">false</span><span class="img"><img src="imgloader.php?delay=1500&<?php print $random; ?>" alt="Image 1500ms delay"/></span></p></li>
-	<li><p><span class="delay">2000ms</span><span class="loaded">false</span><span class="img"><img src="imgloader.php?delay=2000&<?php print $random; ?>" alt="Image 2000ms delay"/></span></p></li>
-	<li><p><span class="delay">2500ms</span><span class="loaded">false</span><span class="img"><img src="imgloader.php?delay=2500&<?php print $random; ?>" alt="Image 2500ms delay"/></span></p></li>
+	<li><p>
+		<span class="head delay">Delay</span>
+		<span class="head img">Image</span>
+		</p></li>
+	<li><p>
+		<span class="delay">500ms</span>
+		<span class="img"><img src="imgloader.php?delay=500&<?php print $random; ?>" alt="Image 500ms delay"/></span>
+	</p></li>
+	<li><p>
+		<span class="delay">1000ms</span>
+		<span class="img"><img src="imgloader.php?delay=1000&<?php print $random; ?>" alt="Image 1000ms delay"/></span>
+	</p></li>
+	<li><p>
+		<span class="delay">1500ms</span>
+		<span class="img"><img src="imgloader.php?delay=1500&<?php print $random; ?>" alt="Image 1500ms delay"/></span>
+	</p></li>
+	<li><p>
+		<span class="delay">2000ms</span>
+		<span class="img"><img src="imgloader.php?delay=2000&<?php print $random; ?>" alt="Image 2000ms delay"/></span>
+	</p></li>
+	<li><p>
+		<span class="delay">2500ms</span>
+		<span class="img"><img src="imgloader.php?delay=2500&<?php print $random; ?>" alt="Image 2500ms delay"/></span>
+	</p></li>
+	<li><p>
+		<span class="delay">5000ms</span>
+		<span class="img"><img src="imgloader.php?delay=5000&<?php print $random; ?>" alt="Image 5000ms delay"/></span>
+	</p></li>
 </ul>
 
 <p><a href="javascript:location.reload();">Reload this page</a></p>
@@ -47,12 +59,9 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script src="pi.imgloaded.js"></script>
 <script>
-$('#all img').imagesLoaded(function (e) {
-	$(this).parent().siblings('span.loaded').text('true');
-}, true);
 
-$('#each img').imagesLoaded(function (e) {
-	$(this).parent().siblings('span.loaded').text('true');
+$('#each img').bind('imgload',  function (e) {
+	$(this).parent().siblings().css('background-color', '#5FD269');
 });
 </script>
 </body>
--- pi.imgloaded.js
+++ pi.imgloaded.js
@@ -9,29 +9,30 @@
 
 
 // Added support for calling the callback on each image load event
-// 
 
-// 'fireOne' argument is optional, if set, will invoke the callback once for every
-// image in the 'this' collection, thus making 'this' in the callback that element alone
-// If it's not used, the callback will be invoked once all the images in the collection has
-// been loaded. And 'this' will be the jQuery collection of the filtered 'img' elements.
-$.fn.imagesLoaded = function(callback, fireOne) {
-  var
-    args = arguments,
-    elems = this.filter('img'),
-    elemsLen = elems.length - 1;
 
-  elems
-    .bind('load', function(e) {
-        if (fireOne) {
-            !elemsLen-- && callback.call(elems, e);
-        } else {
-            callback.call(this, e);
-        }
-    }).each(function() {
-        // cached images don't fire load sometimes, so we reset src.
-        if (this.complete || this.complete === undefined){
-            this.src = this.src;
-        }
-    });
+(function ($) {
+
+var undefined;
+
+$.event.special.imgload = {
+
+	setup: function(data, namespaces, hollaback) {
+		if (this.tagName.toLowerCase() != "img") return false;
+
+		// Bind the actual load event
+		$.event.add(this, 'load', hollaback);
+
+			// Fix for certain browsers not triggering on cached images,  we reset .src to
+			// trigger this
+			if (this.complete || this.complete === undefined) {
+				this.src = this.src;
+			}
+	},
+
+	teardown: function(namespaces) {
+		$.event.remove(this, 'load', hollaback);
+	},
 }
+
+}(jQuery));
