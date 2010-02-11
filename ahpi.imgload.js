c4cf0fe65042094ad38bfcb625d05e734a8cda8b
Andrée Hansson <andree@andree-desktop.(none)>
2010-02-11 22:27:50 +0100

    Fixed an issue where the event callback would fire twice.

--- ahpi.imgload.js
+++ ahpi.imgload.js
@@ -15,11 +15,6 @@
  * Note that you can bind the 'error' event on data uri images, this will trigger when
  * data uri images isn't supported.
  * 
-<<<<<<< HEAD:ahpi.imgload.js
-=======
- * Thanks to Ben Alman (benalman.com) for the heads up on the new Special Event API in 1.4.
- * 
->>>>>>> d76792f6ed61c0d0f056ecce03c18b60f06f3795:ahpi.imgload.js
  * Tested in:
  * FF2-3.6
  * IE6-8
@@ -29,7 +24,6 @@
 (function ($) {
 
 $.event.special.load = {
-<<<<<<< HEAD:ahpi.imgload.js
 	setup: function(data, namespaces, hollaback) {
 		var retVal = false;
 		
@@ -49,24 +43,6 @@ $.event.special.load = {
 		}
 		
 		return retVal;
-=======
-	add: function(hollaback, data) {
-		if (this.tagName.toLowerCase() === 'img') {
-
-			// Image is already complete, fire the hollaback (fixes browser issues were cached
-			// images isn't triggering the load event)
-			if (this.complete || this.readyState === 4) {
-				var e = jQuery.Event('load');
-				e.data = data;
-				hollaback.call(this, e);
-			}
-				
-			// Check if data URI images is supported, fire 'error' event if not.
-			else if (this.readyState === 'uninitialized' && this.src.indexOf('data:') >= 0) {
-				$(this).trigger('error');
-			}
-		}
->>>>>>> d76792f6ed61c0d0f056ecce03c18b60f06f3795:ahpi.imgload.js
 	}
 }
 
