aa3ccf0bfa2b5921c3b4a6cb017378c31518da2c
Andrée Hansson <peolanha@gmail.com>
2010-08-23 22:23:26 +0200

    Fixed a few issues where the event would crash with a recursive error, as well as joining in csnovers jslint and data uri fixes. Thanks to @miketaylr, @ajpiano and csnover(at github) for testing.

--- ahpi.imgload.js
+++ ahpi.imgload.js
@@ -16,34 +16,27 @@
  * data uri images isn't supported.
  * 
  * Tested in:
- * FF2-3.6
- * IE6-8
- * Chromium4 Developer
+ * FF 3+
+ * IE 6-8
+ * Chromium 5-6
  * Opera 9-10
- */ 
+ */
 (function ($) {
+	$.event.special.load = {
+		add: function (hollaback, x, y) {
+			if ( this.nodeType === 1 && this.tagName.toLowerCase() === 'img' && this.src !== '' ) {
 
-$.event.special.load = {
-	setup: function(data, namespaces, hollaback) {
-		var retVal = false;
-		
-		if (this.tagName.toLowerCase() === 'img' && this.src !== "") {
-			// Image is already complete, fire the hollaback (fixes browser issues were cached
-			// images isn't triggering the load event)
-			if (this.complete || this.readyState === 4) {
-				$(this).bind('load', data || {}, hollaback).trigger('load');
-				retVal = true;
-			}
-			
-			// Check if data URI images is supported, fire 'error' event if not
-			else if (this.readyState === 'uninitialized' && this.src.indexOf('data:') >= 0) {
-				$(this).trigger('error');
-				retVal = true;
+				// Image is already complete, fire the hollaback (fixes browser issues were cached
+				// images isn't triggering the load event)
+				if ( this.complete || this.readyState === 4 ) {
+					hollaback.handler.apply(this);
+				}
+
+				// Check if data URI images is supported, fire 'error' event if not
+				else if (this.readyState === 'uninitialized' && this.src.indexOf('data:') === 0) {
+					$(this).trigger('error');
+				}
 			}
 		}
-		
-		return retVal;
-	}
-}
-
+	};
 }(jQuery));
