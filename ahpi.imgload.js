ea365ee97a6103a00f64e7105c8c2beab543b7b1
Jess Thrysoee <jess@thrysoee.dk>
2010-10-23 02:17:25 +0200

    Prevent infinite recursion, and only call event handler once.

--- ahpi.imgload.js
+++ ahpi.imgload.js
@@ -23,21 +23,23 @@
  */
 (function ($) {
 	$.event.special.load = {
+		setup: function () {
+				// Attach to DOM
+				return false;
+		},
 		add: function (hollaback) {
 			if ( this.nodeType === 1 && this.tagName.toLowerCase() === 'img' && this.src !== '' ) {
 				// Image is already complete, fire the hollaback (fixes browser issues were cached
 				// images isn't triggering the load event)
-				if ( this.complete || this.readyState === 4 ) {
+				if ( this.complete || this.readyState === 'complete' ) {
 					hollaback.handler.apply(this);
+					// Only call handler once, even if DOM event fires
+					hollaback.handler = function () {};
 				}
 
 				// Check if data URI images is supported, fire 'error' event if not
 				else if ( this.readyState === 'uninitialized' && this.src.indexOf('data:') === 0 ) {
 					$(this).trigger('error');
-				}
-				
-				else {
-					$(this).bind('load', hollaback.handler);
 				}
 			}
 		}
