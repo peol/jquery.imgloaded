b62626ee2b43aad1aa1a78f94e12b219f0c36688
Jess Thrysoee <jess@thrysoee.dk>
2010-10-23 17:36:53 +0200

    invoke handler again if img.src changes

--- ahpi.imgload.js
+++ ahpi.imgload.js
@@ -3,7 +3,7 @@
  * Needed because some browsers does not trigger the event on cached images.
 
  * MIT License
- * Paul Irish     | @paul_irish | www.paulirish.com
+ * Paul Irish		| @paul_irish | www.paulirish.com
  * Andree Hansson | @peolanha   | www.andreehansson.se
  * 2010.
  *
@@ -21,24 +21,41 @@
  * Chromium 5-6
  * Opera 9-10
  */
+/*global jQuery*/
 (function ($) {
 	$.event.special.load = {
 		setup: function () {
-				// Attach to DOM
-				return false;
+			// Attach to DOM
+			return false;
 		},
-		add: function (hollaback) {
-			if ( this.nodeType === 1 && this.tagName.toLowerCase() === 'img' && this.src !== '' ) {
-				// Image is already complete, fire the hollaback (fixes browser issues were cached
+		add: function (handleObj) {
+			var handler, src;
+
+			if (this.nodeType === 1 && this.tagName.toLowerCase() === 'img' && this.src !== '') {
+
+				// Image is already complete, fire the handler (fixes browser issues were cached
 				// images isn't triggering the load event)
-				if ( this.complete || this.readyState === 'complete' ) {
-					hollaback.handler.apply(this);
-					// Only call handler once, even if DOM event fires
-					hollaback.handler = function () {};
+				if (this.complete || this.readyState === 'complete') {
+
+					handler = handleObj.handler;
+
+					// this wrapper prevents the handler for being invoked twice if loading
+					// completed and an event got queued during this javascript exectution.
+					// The handler is only called again if img.src changes.
+					handleObj.handler = function () {
+
+						// only call handler if src has changed
+						if (src !== this.src) {
+							src = this.src;
+							return handler.apply(this, arguments);
+						}
+					};
+
+					handleObj.handler.call(this, $.Event(handleObj.type));
 				}
 
 				// Check if data URI images is supported, fire 'error' event if not
-				else if ( this.readyState === 'uninitialized' && this.src.indexOf('data:') === 0 ) {
+				else if (this.readyState === 'uninitialized' && this.src.indexOf('data:') === 0) {
 					$(this).trigger('error');
 				}
 			}
