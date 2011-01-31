6a95fe509e33766868dd476d992b42ef96b37d3b
Jeffery To <jeffery.to@gmail.com>
2011-01-31 14:08:36 +0800

    use the local variable el for 'this'

--- ahpi.imgload.js
+++ ahpi.imgload.js
@@ -25,23 +25,23 @@
 	$.event.special.load = {
 		add: function () {
 			var el = this;
-			if ( this.nodeType === 1 && this.nodeName.toUpperCase() === 'IMG' && this.src !== '' ) {
+			if ( el.nodeType === 1 && el.nodeName.toUpperCase() === 'IMG' && el.src !== '' ) {
 				// Image is already complete, fire the handler (fixes browser issues were cached
 				// images isn't triggering the load event)
-				if ( this.complete || this.complete === undefined ) {
+				if ( el.complete || el.complete === undefined ) {
 					// Let jQuery finish binding the event handler
 					setTimeout(function () {
 						var src = el.src;
 						// webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
 						// data uri bypasses webkit log warning (thx doug jones)
-						el.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+						el.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
 						el.src = src;
 					}, 0);
 				}
 
 				// Check if data URI images is supported, fire 'error' event if not
-				else if ( this.readyState === 'uninitialized' && this.src.indexOf('data:') === 0 ) {
-					$(this).trigger('error');
+				else if ( el.readyState === 'uninitialized' && el.src.indexOf('data:') === 0 ) {
+					$(el).trigger('error');
 				}
 			}
 		}
