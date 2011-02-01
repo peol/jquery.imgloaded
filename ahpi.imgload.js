10b7b715e2002ba51cdf0df9832ce71a606a12f9
Jeffery To <jeffery.to@gmail.com>
2011-02-01 16:07:45 +0800

    treat the error event in the same way as load

--- ahpi.imgload.js
+++ ahpi.imgload.js
@@ -1,6 +1,6 @@
 /*
  * Special event for image load events
- * Needed because some browsers does not trigger the event on cached images.
+ * Needed because some browsers does not trigger the event on already-loaded images.
 
  * MIT License
  * Paul Irish     | @paul_irish | www.paulirish.com
@@ -22,39 +22,36 @@
  * Opera 9-10
  */
 (function ($, undefined) {
-	$.event.special.load = {
-		add: function (handleObj) {
-			var el = this, flag = 'load-guid', old_handler;
+	$.each({
+		load: function (el) { return el.complete || el.complete === undefined; },
+		error: function (el) { return el.readyState === 'uninitialized' && el.src.indexOf('data:') === 0; }
+	}, function (eventName, isReady) {
+		$.event.special[eventName] = {
+			add: function (handleObj) {
+				var el = this, flag = eventName + '-guid', old_handler = handleObj.handler;
 
-			if ( el.nodeType === 1 && el.nodeName.toUpperCase() === 'IMG' && el.src !== '' ) {
-				// Image is already complete, fire the handler (fixes browser issues were cached
-				// images isn't triggering the load event)
-				if ( el.complete || el.complete === undefined ) {
-					old_handler = handleObj.handler;
+				if ( el.nodeType === 1 && el.nodeName.toUpperCase() === 'IMG' && el.src !== '' ) {
+					// Check if the image is already loaded or if data URI images are supported
+					// Trigger the event handler if necessary
+					if ( isReady(el) ) {
+						handleObj.handler = function () {
+							var guid = $.data(el, flag), ret;
+							if ( guid === old_handler.guid || guid === undefined ) {
+								ret = old_handler.apply(el, arguments);
+							}
+							return ret;
+						};
 
-					handleObj.handler = function () {
-						var guid = $.data(el, flag), ret;
-						if ( guid === old_handler.guid || guid === undefined ) {
-							ret = old_handler.apply(el, arguments);
-						}
-						return ret;
-					};
-
-					// Let jQuery finish binding the event handler
-					setTimeout(function () {
-						$(el)
-							.data(flag, old_handler.guid)
-							.trigger('load')
-							.removeData(flag);
-					}, 0);
-				}
-
-				// Check if data URI images is supported, fire 'error' event if not
-				else if ( el.readyState === 'uninitialized' && el.src.indexOf('data:') === 0 ) {
-					$(el).trigger('error');
+						// Let jQuery finish binding the event handler
+						setTimeout(function () {
+							$(el)
+								.data(flag, old_handler.guid)
+								.trigger(eventName)
+								.removeData(flag);
+						}, 0);
+					}
 				}
 			}
-
-		}
-	};
+		};
+	});
 }(jQuery));
